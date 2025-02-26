b1ef20cdcd55035dabfcfdd544a9613644da43df
sudo-poweroff <sdellaporta34@gmail.com>
2025-02-26 19:46:30 +0100

    Updated code to check dataset integrity and upload integrity files

--- dataset_integrity.py
+++ dataset_integrity.py
@@ -1,30 +1,31 @@
 from azure_connection import get_azure_connection
 import os
 import csv
+import re
 
 def check_dataset_integrity (container_client, local_directory):
-    # Elenca i file nella directory locale
+    # List files of local directory
     local_files = set()
-    for root, dirs, files in os.walk(local_directory):
+    for root, _, files in os.walk(local_directory):
         for file in files:
             path=os.path.join(local_directory.split('/')[1],os.path.relpath(os.path.join(root, file), local_directory))
             local_files.add(path.replace("\\", "/"))
 
-    # Elenca i file nel contenitore remoto
+    # List files of remote directory
     remote_files = set()
+    pattern=r".*\.[a-zA-Z0-9]+$"
     for blob in container_client.list_blobs():
         if blob.name.split('/')[0] == os.path.basename(local_directory):
-            if blob.name.endswith('.gz') or blob.name.endswith('.json') or blob.name.endswith('.bval') or blob.name.endswith('.bvec') or blob.name.endswith('.tsv') or blob.name.endswith('.md') or blob.name.endswith('CHANGES') or blob.name.endswith('README') or blob.name.endswith('.eeg') or blob.name.endswith('.vhdr') or blob.name.endswith('.txt') or blob.name.endswith('.vmrk') or blob.name.endswith('.sfp') :
+            if re.match(pattern,blob.name) or blob.name.endswith('CHANGES') or blob.name.endswith('README') or blob.name.endswith('LICENSE') or blob.name.endswith('RECORDS') or blob.name.endswith('SHA256SUMS'):
                 remote_files.add(blob.name)
 
 
-    # Confronta i file locali e remoti
+    # Check integrity
     common_files = local_files.intersection(remote_files)
     only_in_local = local_files - remote_files
     only_in_remote = remote_files - local_files
 
-    # Salva i risultati in un file CSV
-    filename = os.path.basename(local_directory) + "_integrity_check.csv"
+    filename = os.path.basename(local_directory) + "/integrity_check.csv"
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["File", "Location"])
@@ -39,7 +40,10 @@ def check_dataset_integrity (container_client, local_directory):
             writer.writerow([file, "Remote"])
 
 
-container_client = get_azure_connection()
+if __name__ == "__main__":
+    container_name = input("Enter the container name: ")
+    connection_string = input("Enter the connection string: ") 
+    container_client = get_azure_connection(container_name=container_name, connection_string=connection_string)
 
-local_directory = input("Enter the dataset path (with /): ")
-check_dataset_integrity(container_client, local_directory)
+    local_directory = input("Enter the dataset path (with /): ")
+    check_dataset_integrity(container_client=container_client, local_directory=local_directory)
