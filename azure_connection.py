fc2cf410e222f770ef973d1bd86245bacd208879
sudo-poweroff <sdellaporta34@gmail.com>
2025-02-26 19:37:14 +0100

    Update azure_connection

--- azure_connection.py
+++ azure_connection.py
@@ -1,20 +1,14 @@
 from azure.storage.blob import BlobServiceClient
-import os
-import csv
 
-def get_azure_connection ():
-    # Configura la connessione
+def get_azure_connection (container_name,connection_string):
     account_name = "thesislakehouse"
-    container_name = "rawdata"
-    #Azure connection string
-    connection_string = ""
+    container_name = container_name
+    connection_string = connection_string
     
-    
-    print("Connection to Azure...")
-    # Crea il BlobServiceClient
+    print("Connection to Azure "+container_name+" container...")
+    # Create BlobServiceClient
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-    # Crea un ContainerClient
+    # Create ContainerClient
     container_client = blob_service_client.get_container_client(container_name)
     print("Connection established!")
     return container_client
